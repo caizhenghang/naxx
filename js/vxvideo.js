68d4e0add479d23f604faf5117aa4d63fd21ae09
张三 <1787821427@qq.com>
2024-02-20 12:34:39 +0800

    first commit

---
+++ js/vxvideo.js
@@ -0,0 +1,41 @@
+var html='<style>.overlay{color:#fff;}.overlay #share{border:solid 1px #c43c35;background-color: #ca3535;padding:10px;color: #fff;background-image: -webkit-linear-gradient(top, rgba(255,255,255,.3), rgba(255,255,255,0));}</style>\
+<div class="overlay" style="position:fixed;top:0px;left:0px;z-index: 10001;display:none;width:100%;height:100%;text-align:center;font-size:18px;font-weight:bold;background-color:rgba(0,0,0,0.5);">\
+<div>\
+<br>\
+<br\
+<span>寰俊闄愬埗闀胯棰戝垎浜�</span>\
+<br>\
+<span>鐐逛笅闈㈢殑澶嶅埗 鍐嶅幓寰俊澶栨墦寮€</span>\
+<hr>\
+<span>https://177.sk</span>\
+<button id="share" class="btn btn-info">鐐瑰嚮澶嶅埗</button>\
+<hr>\
+<div>涓嶈鐐� [鍙充笂瑙� ...] 鎵撲笉寮€</div>\
+<br>\
+</div>\
+</div>';
+if(navigator.userAgent.toLowerCase().match(/micromessenger/i)=="micromessenger"){
+    // if($('#vxvideo').length>0){
+    //     var topath=$.trim($('#vxvideo').data("to"));
+    //     $('#vxvideo').append(html);
+        
+    //     var siid = setInterval(function(){
+    //         if(dPlayers[0].video.currentTime>30){
+    //             dPlayers[0].video.pause();
+    //             $(".overlay").show();
+    //         }
+    //     },100);
+    //     $("#share").click(function(){
+    //         $(this).text("澶嶅埗鎴愬姛");
+    //     });
+    //     new ClipboardJS("#share", {
+    //         text: function(trigger) {
+    //             return window.location.origin+"/"+topath+"?from=vx2";
+    //         }
+    //     });
+    // }
+    
+     $(document).ready(function(){
+        //$("a[href='/qu/1']").attr("href","https://www.baidu.com/s?wd=%E8%AF%B7%E5%88%B0%E6%9C%80%E6%96%B0%E5%9C%B0%E5%9D%80%20178.sk%20%E6%89%93%E5%BC%80");
+    });
+}
